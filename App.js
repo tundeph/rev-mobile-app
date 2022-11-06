d0d2650f746dbc6da320b2de43b97555b8e2b0b5
tundeph <54335828+tundeph@users.noreply.github.com>
2022-11-06 18:17:28 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+import Todos from "./pages/todos/Todos"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Todos />
+		</View>
+	)
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		// justifyContent: "center",
+	},
+})
