a7739146092d5a961ed6e9448065fbd8f4b4f322
tundeph <54335828+tundeph@users.noreply.github.com>
2022-11-09 20:18:33 +0000

    Update Header.js

--- pages/todos/components/Header.js
+++ pages/todos/components/Header.js
@@ -1,19 +1,17 @@
 import React from "react"
 import styled from "styled-components/native"
-import { Text, View } from "react-native"
+import { Text, View, SafeAreaView } from "react-native"
 
 const Container = styled(View)`
 	width: 100%;
 	padding-top: 30px;
 	padding-bottom: 20px;
 	background-color: #592be3;
-	/* align-items: center; */
 `
 
 const HeaderText = styled(Text)`
-	font-weight: 900;
+	font-weight: 700;
 	font-size: 20px;
-	text-transform: uppercase;
 	text-align: center;
 	color: #fff;
 `
@@ -21,7 +19,9 @@ const HeaderText = styled(Text)`
 const Header = () => {
 	return (
 		<Container>
-			<HeaderText> Todo </HeaderText>
+			<SafeAreaView>
+				<HeaderText> React Native App </HeaderText>
+			</SafeAreaView>
 		</Container>
 	)
 }
