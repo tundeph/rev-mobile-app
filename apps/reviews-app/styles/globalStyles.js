ef8e186cdedad12cc112a0bc16766275eb8938c7
tundeph <54335828+tundeph@users.noreply.github.com>
2022-11-23 18:16:05 +0000

    Create globalStyles.js

---
+++ apps/reviews-app/styles/globalStyles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components/native"
+import { View, Text } from "react-native"
+
+export const TitleText = styled(Text)`
+	font-family: "Beatrice Bold";
+	font-size: 18px;
+`
+
+export const BodyText = styled(Text)`
+	font-family: "Beatrice";
+`
+
+export const ContainerView = styled(View)`
+	flex: 1;
+	padding: 24px;
+`
