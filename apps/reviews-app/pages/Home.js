8184b734d06fbe1f9478ce36595c1296275d8c88
tundeph <54335828+tundeph@users.noreply.github.com>
2022-11-23 18:15:28 +0000

    Create Home.js

---
+++ apps/reviews-app/pages/Home.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react"
+import { Button, FlatList, TouchableOpacity } from "react-native"
+import { ContainerView, TitleText, BodyText } from "../styles/globalStyles"
+
+const Home = ({ navigation }) => {
+	const [reviews, setReviews] = useState([
+		{ title: "Sniper Revolution", rating: 3, body: "Lorem ipsum", key: "1" },
+		{ title: "Game of Thrones", rating: 5, body: "Lorem ipsum", key: "2" },
+		{ title: "Last Kingdom", rating: 4, body: "Lorem ipsum", key: "3" },
+	])
+
+	return (
+		<ContainerView>
+			<TitleText>Home </TitleText>
+			<FlatList
+				data={reviews}
+				renderItem={({ item }) => (
+					<TouchableOpacity
+						onPress={() => navigation.navigate("Review Details", item)}
+					>
+						<BodyText> {item.title} </BodyText>
+					</TouchableOpacity>
+				)}
+			/>
+		</ContainerView>
+	)
+}
+
+export default Home
