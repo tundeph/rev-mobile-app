86f47e4e2181e7ad315867a2332d0207e1f05830
tundeph <54335828+tundeph@users.noreply.github.com>
2023-01-14 19:13:05 +0000

    Update ReviewDetails.js

--- apps/reviews-app/pages/ReviewDetails.js
+++ apps/reviews-app/pages/ReviewDetails.js
@@ -1,15 +1,14 @@
 import React from "react"
 import { ContainerView, TitleText, BodyText } from "../styles/globalStyles"
 
-const Home = ({ route }) => {
+const ReviewDetails = ({ route }) => {
 	const { title, body } = route.params
 
 	return (
 		<ContainerView>
-			<TitleText> {title}</TitleText>
 			<BodyText> {body}</BodyText>
 		</ContainerView>
 	)
 }
 
-export default Home
+export default ReviewDetails
